f5eb4362b313b8bb59bcdb66105557c171a8c750
Michal Drabik <micdra7@gmail.com>
2021-07-05 17:40:14 +0200

    chore: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Engineering project')
+    .setDescription('Engineering project API description')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
