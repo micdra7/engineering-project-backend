5bb26827938e435d7acc4fbdbf2e940706ef691a
Michal Drabik <micdra7@gmail.com>
2021-07-05 17:34:50 +0200

    chore: typeorm initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './utils/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,7 +1,9 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 5000,
   database: {
-    url: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   },
