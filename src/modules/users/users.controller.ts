f4f15cffd0b70488a91fee944b1e857802fbee97
Michal Drabik <micdra7@gmail.com>
2021-07-06 18:50:43 +0200

    feat: add account creation

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -18,6 +19,11 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/register')
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    return this.usersService.register(registerUserDto);
   }
 
   @Get()
