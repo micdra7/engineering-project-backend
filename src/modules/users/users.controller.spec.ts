80a2ee4163c051dd2c11675a68d26aeef47fccfc
Michal Drabik <micdra7@gmail.com>
2021-07-06 19:39:08 +0200

    feat: add basic register tests for users controller

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,20 +1,90 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { RegisterUserResponse } from './response/register-user.response';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            register: jest
+              .fn()
+              .mockImplementation(
+                (dto: RegisterUserDto): Promise<RegisterUserResponse> => {
+                  if (!dto) {
+                    throw new HttpException(
+                      'Bad request',
+                      HttpStatus.BAD_REQUEST,
+                    );
+                  }
+
+                  return Promise.resolve({
+                    id: 1,
+                    email: dto.email,
+                    firstName: dto.firstName,
+                    lastName: dto.lastName,
+                    isActive: true,
+                    workspace: {
+                      id: 1,
+                      name: dto.workspaceName,
+                    },
+                  });
+                },
+              ),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('register - should create and assign user to workspace', async () => {
+    const dto: RegisterUserDto = {
+      email: 'test@test.net',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'MyVerySecretPassword123$',
+      workspaceName: 'Test workspace',
+    };
+
+    const expectedResult: RegisterUserResponse = {
+      id: 1,
+      email: dto.email,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      isActive: true,
+      workspace: {
+        id: 1,
+        name: dto.workspaceName,
+      },
+    };
+
+    const actualResult: RegisterUserResponse = await controller.register(dto);
+
+    expect(usersService.register).toBeCalled();
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('register - should throw for empty body', async () => {
+    const dto: RegisterUserDto = null;
+
+    expect(async () => {
+      await controller.register(dto);
+    }).rejects.toThrow(HttpException);
   });
 });
