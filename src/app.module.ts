b7a08f487ccfb0f35b14debc9da60f640670ac19
Michal Drabik <micdra7@gmail.com>
2021-07-06 17:49:51 +0200

    feat: add needed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CallsModule } from './modules/calls/calls.module';
+import { ChatroomsModule } from './modules/chatrooms/chatrooms.module';
+import { GamesModule } from './modules/games/games.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { UsersModule } from './modules/users/users.module';
+import { WorkspacesModule } from './modules/workspaces/workspaces.module';
 import configuration from './utils/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     TypeOrmModule.forRoot(),
+    UsersModule,
+    WorkspacesModule,
+    CallsModule,
+    ChatroomsModule,
+    GamesModule,
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Workspace } from '../workspaces/entities/workspace.entity';
+import { UserWorkspaces } from '../workspaces/entities/userWorkspaces.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Workspace, UserWorkspaces])],
   controllers: [UsersController],
   providers: [UsersService],
 })
