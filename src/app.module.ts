82bdfd69a7246d6a1cf819739d83173342db5fa3
Michal Drabik <micdra7@gmail.com>
2021-07-07 17:50:38 +0200

    feat: remove uneeded app controller, service and their tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CallsModule } from './modules/calls/calls.module';
 import { ChatroomsModule } from './modules/chatrooms/chatrooms.module';
 import { GamesModule } from './modules/games/games.module';
@@ -22,7 +20,5 @@ import configuration from './utils/configuration';
     GamesModule,
     TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
