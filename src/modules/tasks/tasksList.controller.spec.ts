522118e8d5eb8a419795c54819798bc46637e5aa
Michal Drabik <micdra7@gmail.com>
2021-08-17 19:30:29 +0200

    feat: add unit tests for task list controller

--- src/modules/tasks/tasksList.controller.spec.ts
+++ src/modules/tasks/tasksList.controller.spec.ts
@@ -1,7 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { PaginationResponse } from '../../utils/pagination.response';
 import { Workspace } from '../workspaces/entities/workspace.entity';
+import { CreateTaskListDto } from './dto/create-taskList.dto';
+import { UpdateTaskListDto } from './dto/update-taskList.dto';
 import { TaskList } from './entities/taskList.entity';
+import { TaskItemResponse } from './response/task-item.response';
 import { TasksListsController } from './tasksList.controller';
 import { TaskListsService } from './tasksList.service';
 
@@ -17,7 +21,11 @@ describe('TasksListsController', () => {
           provide: getRepositoryToken(TaskList),
           useValue: {
             findOne: jest.fn().mockImplementation((...args: any) => {
-              if (args?.where?.name === 'Test List') {
+              if (
+                args?.[0]?.where?.name === 'Test List' ||
+                args?.[0]?.where?.id === 1 ||
+                args?.[0] === 1
+              ) {
                 return Promise.resolve({
                   id: 1,
                   name: 'Test List',
@@ -28,7 +36,6 @@ describe('TasksListsController', () => {
             }),
             create: jest.fn().mockImplementation(({ name }) => {
               return Promise.resolve({
-                id: 1,
                 name,
               });
             }),
@@ -38,13 +45,33 @@ describe('TasksListsController', () => {
                 name,
               });
             }),
+            remove: jest.fn().mockImplementationOnce(() => Promise.resolve()),
+            createQueryBuilder: jest.fn().mockReturnValue({
+              innerJoinAndSelect: () => ({
+                where: () => ({
+                  orderBy: () => ({
+                    skip: () => ({
+                      take: () => ({
+                        getManyAndCount: () => [
+                          [{ id: 1, name: 'Test List' }],
+                          1,
+                        ],
+                      }),
+                    }),
+                  }),
+                }),
+              }),
+            }),
           },
         },
         {
           provide: getRepositoryToken(Workspace),
           useValue: {
-            findOne: jest.fn().mockImplementation((...args: any) => {
-              if (args?.where?.name === 'Test Workspace') {
+            findOne: jest.fn().mockImplementation((args: any) => {
+              if (
+                args?.where?.name === 'Test Workspace' ||
+                args?.[0]?.where?.name === 'Test Workspace'
+              ) {
                 return Promise.resolve({
                   id: 1,
                   name: 'Test Workspace',
@@ -57,6 +84,7 @@ describe('TasksListsController', () => {
 
               return Promise.resolve(null);
             }),
+            save: jest.fn(),
           },
         },
       ],
@@ -67,5 +95,73 @@ describe('TasksListsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('create - should create a new task list', async () => {
+    const dto: CreateTaskListDto = {
+      name: 'Test List 123',
+    };
+
+    const expected: TaskItemResponse = {
+      id: 1,
+      name: 'Test List 123',
+    };
+
+    const actual = await controller.create(dto, {
+      user: { workspaceName: 'Test Workspace' },
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('getAll - returns a list of all task lists', async () => {
+    const expected: PaginationResponse<TaskItemResponse> = {
+      data: [{ id: 1, name: 'Test List' }],
+      meta: {
+        currentPage: 1,
+        itemCount: 10,
+        totalPages: 1,
+        totalItems: 1,
+      },
+    };
+
+    const actual = await controller.getAll(1, 10, {
+      user: { workspaceName: 'Test List' },
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('getOne - returns one taskList', async () => {
+    const expected: TaskItemResponse = {
+      id: 1,
+      name: 'Test List',
+    };
+
+    const actual = await controller.getOne(1, {
+      user: { workspaceName: 'Test List' },
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('update - updates one task', async () => {
+    const dto: UpdateTaskListDto = {
+      id: 1,
+      name: 'Test List 123123',
+    };
+
+    const expected: TaskItemResponse = {
+      id: 1,
+      name: 'Test List 123123',
+    };
+
+    const actual = await controller.update(dto);
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('delete - should delete if taskList with given id exists', async () => {
+    await expect(controller.delete('1')).resolves.not.toThrow();
   });
 });
