571baafc5d7d55646b7c90bb7ff4df1270e46b4b
Michal Drabik <micdra7@gmail.com>
2021-08-18 20:45:17 +0200

    feat: update task controller methods

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,6 +1,30 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Req,
+  Query,
+  DefaultValuePipe,
+  ParseIntPipe,
+  Param,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskItemResponse } from './response/task-item.response';
+import { ApiPaginatedResponse } from '../../utils/pagination.decorator';
+import { PaginationResponse } from '../../utils/pagination.response';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
+import { ChangeListDto } from './dto/change-list.dto';
 
 @ApiTags('Tasks')
 @Controller('tasks')
@@ -8,37 +32,66 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  async create() {
-    // creates a new task
+  @ApiCreatedResponse({
+    description: 'Task successfully created',
+    type: TaskItemResponse,
+  })
+  @ApiBadRequestResponse({ description: 'Failed validation' })
+  async create(
+    @Body() dto: CreateTaskDto,
+    @Req() req,
+  ): Promise<TaskItemResponse> {
+    return this.tasksService.create(dto, req.user.workspaceName);
   }
 
   @Get()
-  async getAll() {
-    // returns all task
+  @ApiPaginatedResponse(TaskItemResponse, 'List is successfully fetched')
+  async getAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+    @Req() req,
+  ): Promise<PaginationResponse<TaskItemResponse>> {
+    return this.tasksService.findAll(req.user.workspaceName, page, limit);
   }
 
   @Get()
-  async getOne() {
-    // returns one task
+  @ApiOkResponse({
+    description: 'Returns selected task',
+    type: TaskItemResponse,
+  })
+  @ApiBadRequestResponse()
+  async getOne(@Param('id') id: string): Promise<TaskItemResponse> {
+    return this.tasksService.findOne(+id);
   }
 
   @Patch('/:id')
-  async update() {
-    // updates task
+  @ApiOkResponse({
+    description: 'Updates task for given id',
+    type: TaskItemResponse,
+  })
+  @ApiBadRequestResponse({ description: 'Failed validation' })
+  async update(@Body() dto: UpdateTaskDto): Promise<TaskItemResponse> {
+    return this.tasksService.update(dto);
   }
 
   @Delete('/:id')
-  async delete() {
-    // deletes task
+  @ApiOkResponse({ description: 'Task was removed' })
+  @ApiBadRequestResponse()
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.tasksService.remove(+id);
   }
 
   @Patch('/:id/update-status')
-  async updateStatus() {
-    // updates task status - only for subtasks
+  @ApiOkResponse({ description: 'Status was updated' })
+  @ApiBadRequestResponse({ description: 'Task is not a subtask' })
+  async updateStatus(@Body() dto: UpdateStatusDto): Promise<TaskItemResponse> {
+    return this.tasksService.updateStatus(dto);
   }
 
   @Patch('/:id/change-list')
-  async changeList() {
-    // moves task between lists
+  @ApiOkResponse({ description: 'List was changed' })
+  @ApiBadRequestResponse()
+  async changeList(@Body() dto: ChangeListDto): Promise<TaskItemResponse> {
+    return this.tasksService.changeList(dto);
   }
 }
