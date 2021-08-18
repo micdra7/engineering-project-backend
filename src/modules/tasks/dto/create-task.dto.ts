7e65736cc749a2e4134ab6c3147f3b646aa50ae6
Michal Drabik <micdra7@gmail.com>
2021-08-18 12:26:33 +0200

    feat: enable transforms for date

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTaskDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+
+export class CreateTaskDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty()
+}
