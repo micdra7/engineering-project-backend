b2f590409ebb3839d30d97ddcd5083bfb9861e45
Michal Drabik <micdra7@gmail.com>
2021-10-04 15:32:55 +0200

    feat: add repositories to call service

--- src/modules/calls/calls.module.ts
+++ src/modules/calls/calls.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { CallsService } from './calls.service';
 import { CallsController } from './calls.controller';
 import { CallsGateway } from './calls.gateways';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Call } from './entities/call.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Call, User])],
   controllers: [CallsController],
   providers: [CallsService, CallsGateway],
 })
--- src/modules/calls/calls.service.ts
+++ src/modules/calls/calls.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
 import { CreateCallDto } from './dto/create-call.dto';
 import { UpdateCallDto } from './dto/update-call.dto';
+import { Call } from './entities/call.entity';
 
 @Injectable()
 export class CallsService {
+  constructor(
+    @InjectRepository(Call)
+    private callRepository: Repository<Call>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
   create(createCallDto: CreateCallDto) {
     return 'This action adds a new call';
   }
