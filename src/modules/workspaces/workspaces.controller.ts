536af42941f8a460a2d5af7d0db728411e2c3eda
Michal Drabik <micdra7@gmail.com>
2021-10-21 19:29:11 +0200

    chore: remove unused endpoints

--- src/modules/workspaces/workspaces.controller.ts
+++ src/modules/workspaces/workspaces.controller.ts
@@ -1,18 +1,12 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   HttpCode,
   HttpStatus,
   Req,
 } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
-import { CreateWorkspaceDto } from './dto/create-workspace.dto';
-import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
 import { AddToWorkspaceDto } from './dto/add-to-workspace.dto';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
@@ -20,11 +14,6 @@ import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 @Controller('workspaces')
 export class WorkspacesController {
   constructor(private readonly workspacesService: WorkspacesService) {}
-
-  @Post()
-  create(@Body() createWorkspaceDto: CreateWorkspaceDto) {
-    return this.workspacesService.create(createWorkspaceDto);
-  }
 
   @Post('/add-user')
   @ApiOkResponse({ description: 'User added to workspace' })
@@ -34,28 +23,5 @@ export class WorkspacesController {
       req.user.workspaceName,
       dto,
     );
-  }
-
-  @Get()
-  findAll() {
-    return this.workspacesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.workspacesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateWorkspaceDto: UpdateWorkspaceDto,
-  ) {
-    return this.workspacesService.update(+id, updateWorkspaceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.workspacesService.remove(+id);
   }
 }
