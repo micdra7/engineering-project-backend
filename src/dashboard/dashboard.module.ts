041f826957beff4c46d8cedf0abffc8ec1005966
Michal Drabik <micdra7@gmail.com>
2021-10-29 18:59:20 +0200

    feat: WIPO dashboard service

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Call } from 'src/modules/calls/entities/call.entity';
+import { Task } from 'src/modules/tasks/entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Call, Task])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
